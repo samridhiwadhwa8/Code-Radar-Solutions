4b82a0043db16328cf0fcc5aa8571135361c4940
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2025-03-29 00:45:46 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,14 +4,14 @@ int fibonacciSeries(int n){
     int a=0;
     int b=1;
     printf("%d",b);
-    if(n==1){
-        return;
-    }
+    // if(n==1){
+    //     return;
+    // }
     printf("%d",a);
     for(int i=3;i<=n;i++){
         int c=a+b;
         b=a;
         a=c;
-        printf("%d",c);
+        printf("%d ",c);
     }
 }
