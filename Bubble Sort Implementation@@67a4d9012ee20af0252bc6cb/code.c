ebd87456e4943157ea33fb53658935ee977d01fc
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2025-04-01 02:16:41 +0530

    Solution to Bubble Sort Implementation

--- Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
+++ Bubble Sort Implementation@@67a4d9012ee20af0252bc6cb/code.c
@@ -11,9 +11,9 @@ int bubbleSort(int arr[], int n){
     }
 }
 int printArray(int arr[], int n){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
+    // int n;
+    // scanf("%d",&n);
+    // int arr[n];
     for(int i=0;i<n;i++){
         scanf("%d",&arr[i]);
     }
