047ae443cc7b0bc7334783444caee397b561524b
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2025-04-01 11:59:51 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -3,8 +3,8 @@
 int selectionSort(int arr[] ,int n){
     for(int i=0;i<n-1;i++){
         int min=i;
-        for(int j=i;j<=n;j++){
-            if(arr[j]>arr[j+1]){
+        for(int j=i+1;j<=n;j++){
+            if(arr[j]<arr[min]){
                 min=j;
             }
         }
