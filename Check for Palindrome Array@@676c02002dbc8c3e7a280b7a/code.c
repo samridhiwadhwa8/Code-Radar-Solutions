2a9795eb9eef3fb67af7767a9c4aedcd2b4900db
Samridhi Wadhwa <samridhiwadhwa49@gmail.com>
2025-04-01 11:12:20 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int rev(int n, int arr[]){
     for(int i=0;i<n/2;i++){
-        int t=arr[i];
+        int tem=arr[i];
         arr[i]=arr[n-i-1];
-        arr[n-i-1]=t;
+        arr[n-i-1]=tem;
     }
     return 1;
 }
+
 int main(){
     int n;
     scanf("%d",&n);
@@ -16,19 +17,31 @@ int main(){
         og[i]=arr[i];
     }
     int res=rev(n,arr);
+    // if(res==1){
+    //     int isp=1;
+    //     for(int i=0;i<n;i++){
+    //         if(arr[i]!=og[i]){
+    //             isp=0;
+    //             break;
+    //         }
+    //     }
+    //     if(isp){
+    //             printf("YES");
+    //         }
+    //         else{
+    //             printf("NO");
+    //         }
+    // }
     if(res==1){
         int isp=1;
-        for(int i=0;i<n;i++){
-            if(arr[i]!=og[i]){
-                isp=0;
-                break;
-            }
+        if(arr[i]!=og[i]){
+            isp=0;
+            break;
         }
-        if(isp){
-                printf("YES");
-            }
-            else{
-                printf("NO");
-            }
+    }
+    if(isp){
+        printf("YES");
+    }else{
+        printf("NO");
     }
 }
